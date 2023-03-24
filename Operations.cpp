93dd45290e7168db30a71a54d364902b8876b8b4
TJ2806 <teojoly28@gmail.com>
2023-03-24 09:17:26 +0100

    Operations
    Ajout de multiplication et de division

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,20 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+
+int multipliction(int a, int b)
+{
+	int resultat;
+	
+	resultat = a*b;
+	
+	return resultat;
+}
+int division(int a, int b)
+{
+	int resultat;
+	
+	resultat = a/b;
+	
+	return resultat;
+}
